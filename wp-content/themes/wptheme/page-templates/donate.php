5e063c5085df3cb0ab892f292a65882b365ee4d2
RattleInGlasses <S44k4r@gmail.com>
2015-11-30 16:58:21 +0300

    Добавление транслитерации назначения платежа.

--- wp-content/themes/wptheme/page-templates/donate.php
+++ wp-content/themes/wptheme/page-templates/donate.php
@@ -24,10 +24,10 @@ Template Name: Donation Form
     }
 
     $paymentPurpose = DonationUtils::createPaymentPurpose($child, $orphanage);
+    $paymentPurposeTranslit = DonationUtils::createTransliteratedPaymentPurpose($child, $orphanage);
 
     get_header();
 ?>
-
 <div class="donate-form">
     <div class="container">
         <div class="form-group">
@@ -35,10 +35,10 @@ Template Name: Donation Form
             <div class="input-block">
                 <form action="https://www.sandbox.paypal.com/cgi-bin/webscr" method="post" target="_top">
                     <input type="hidden" name="cmd" value="_s-xclick">
-                    <input type="hidden" name="hosted_button_id" value="YB67SCGQGLMXW">
+                    <input type="hidden" name="hosted_button_id" value="22S2S5FGJ7GML">
                     <input type="hidden" name="return" value=http://warmhouse-wp.local/donation/">
                     <input type="hidden" name="cancel_return" value="http://warmhouse-wp.local/donation/">
-                    <input type="hidden" name="item_name" value="<?= $paymentPurpose; ?>">
+                    <input type="hidden" name="item_name" value="<?= $paymentPurposeTranslit; ?>">
                     <input type="image" src="http://sozvezdieserdec.ru/uploads/images/knopki/donation.png" border="0" name="submit" alt="PayPal — более безопасный и легкий способ оплаты через Интернет!">
                     <img alt="" border="0" src="https://www.sandbox.paypal.com/ru_RU/i/scr/pixel.gif" width="1" height="1">
                 </form>
@@ -46,5 +46,4 @@ Template Name: Donation Form
         </div>
     </div>
 </div>
-
 <?php get_footer(); ?>
