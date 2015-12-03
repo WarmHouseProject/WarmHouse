4fabb43777fbf886f0a4ccdf9bc1038cdc934273
RattleInGlasses <S44k4r@gmail.com>
2015-12-03 16:00:12 +0300

    Добавление на страницу "Thank you" пояснительного текста

--- wp-content/themes/wptheme/page-templates/thank-you-page.php
+++ wp-content/themes/wptheme/page-templates/thank-you-page.php
@@ -8,6 +8,10 @@ get_header();
 <div class="thank-you-form">
     <div class="container">
         <img width="90%" src="<?= get_template_directory_uri() . '/images/thank-you.png' ?>">
+        <label>
+            Благодарим за оказанную помощь. Ваше пожертвование совершено, и квитанция на пожертвование отправлена вам по электронной почте.
+            Вы можете войти в свою учетную запись по адресу <a href="http://www.paypal.com/ru">www.paypal.com/ru</a> и просмотреть данные этой операции.
+        </label>
     </div>
 </div>
 <?php get_footer(); ?>
