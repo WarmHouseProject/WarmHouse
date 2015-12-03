6a725278058e715479c0625ab801c5869a0d96b2
RattleInGlasses <S44k4r@gmail.com>
2015-12-03 15:13:35 +0300

    Добавлене файлов, необходимых для работы страниц "Thank you" и "Donation"

---
+++ wp-content/themes/wptheme/page-templates/thank-you-page.php
@@ -0,0 +1,13 @@
+<?php
+/*
+Template Name: Thank You Form
+*/
+
+get_header();
+?>
+<div class="thank-you-form">
+    <div class="container">
+        <img width="90%" src="<?= get_template_directory_uri() . '/images/thank-you.png' ?>">
+    </div>
+</div>
+<?php get_footer(); ?>
