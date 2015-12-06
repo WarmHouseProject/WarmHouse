57249fc0fa3035ca1409d092d3edd99afa2041f0
Aleksandr Krylov <Alexandrk86@gmail.com>
2015-12-06 21:51:51 +0300

    Добавил заголовок без картинки

--- wp-content/themes/wptheme/page-templates/full-width.php
+++ wp-content/themes/wptheme/page-templates/full-width.php
@@ -16,7 +16,7 @@
 
 get_header(); ?>
 
-	<div id="primary" class="site-content">
+	<div id="primary" class="services">
 		<div id="content" role="main">
 
 			<?php while ( have_posts() ) : the_post(); ?>
--- wp-content/themes/wptheme/search.php
+++ wp-content/themes/wptheme/search.php
@@ -18,14 +18,14 @@ get_header(); ?>
 				<h1 class="page-title"><?php printf( __( 'Search Results for: %s', 'wptheme' ), '<span>' . get_search_query() . '</span>' ); ?></h1>
 			</header>
 
-			<?php twentytwelve_content_nav( 'nav-above' ); ?>
+			<?php wptheme_content_nav( 'nav-above' ); ?>
 
 			<?php /* Start the Loop */ ?>
 			<?php while ( have_posts() ) : the_post(); ?>
 				<?php get_template_part( 'content', get_post_format() ); ?>
 			<?php endwhile; ?>
 
-			<?php twentytwelve_content_nav( 'nav-below' ); ?>
+			<?php wptheme_content_nav( 'nav-below' ); ?>
 
 		<?php else : ?>
 
