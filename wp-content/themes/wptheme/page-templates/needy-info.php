05b3472296fba1dce479b94e7ff1d52e32636406
Andrey540 <eas540@mail.ru>
2015-12-14 00:29:24 +0300

    redesign Поправил стили, удалил лишние файлы

--- wp-content/themes/wptheme/page-templates/needy-info.php
+++ wp-content/themes/wptheme/page-templates/needy-info.php
@@ -11,33 +11,33 @@ require_once(ABSPATH . WPINC . '/lib/helper/class-request-helper.php');
 
 $needyId   = RequestHelper::getParameter("needy_id");
 $needyType = RequestHelper::getParameter("needy_type");
-$needyItem = null;
+$child = null;
 $editPageController = null;
 $donateHiddenId = null;
 
 if ($needyId && $needyType == NeedyType::CHILD)
 {
-    $needyItem = ChildDBUtils::getChildById($needyId);
+    $child = ChildDBUtils::getChildById($needyId);
     $editPageController = '/edit-child-page-controller.php?child_id=' . $needyId;
     $donateHiddenId = "child_id";
 }
 elseif ($needyId && $needyType == NeedyType::ORPHANAGE)
 {
-    $needyItem = OrphanageDBUtils::getOrphanageById($needyId);
+    $child = OrphanageDBUtils::getOrphanageById($needyId);
     $editPageController = '/edit-orphanage-page-controller.php?orphanage_id=' . $needyId;
     $donateHiddenId = "orphanage_id";
 }
 
 get_header(); ?>
 
-<?php if ($needyItem): ?>
+<?php if ($child): ?>
     <div class="child-info">
         <div class="container">
             <div class="col">
                 <div class="col-md-3">
                     <div class="inner-container image">
                         <div class="inner-image-block">
-                            <img src="<?= get_site_url() . ImageDBUtils::getImageLinkByImageId($needyItem->image_id) ?>" class="img-thumbnail child-image" alt="<?= $needyItem->name ?>">
+                            <img src="<?= get_site_url() . ImageDBUtils::getImageLinkByImageId($child->image_id) ?>" class="img-thumbnail child-image" alt="<?= $child->name ?>">
                         </div>
                     </div>
                     <?php if (is_user_logged_in()): ?>
@@ -48,27 +48,23 @@ get_header(); ?>
                 </div>
                 <div class="col-md-9">
                     <div class="inner-container heading">
-                        <h2 class="head"><?= $needyItem->name ?></h2>
+                        <h2 class="head"><?= $child->name ?></h2>
                     </div>
                     <form class="inner-container heading" action="/donation/" method="post">
-                        <div class="well short-description"><?= $needyItem->short_description ?></div>
-                        <div class="well well-sm contact-info"><?= $needyItem->contact_info ?></div>
-                        <button type="button" id="showLongDescriptionButton" class="btn btn-primary long-description">Читать больше</button>
+                        <div class="well short-description"><?= $child->short_description ?></div>
+                        <div class="well well-sm contact-info"><?= $child->contact_info ?></div>
                         <input type="hidden" name="<?= $donateHiddenId ?>" value="<?= $needyId; ?>">
-                        <?php if (!isset($needyItem->status ) || $needyItem->status != NeedyStatus::HELPED): ?>
+                        <?php if (!isset($child->status ) || $child->status != NeedyStatus::HELPED): ?>
                             <button type="submit" class="btn btn-success donate">Пожертвовать</button>
                         <?php endif; ?>
-                        <div class="well well-lg collapse long-description" id="longDescription"><?= $needyItem->long_description ?></div>
                     </form>
-                    <script>
-                        jQuery(document).ready(function($) {
-                            $('#showLongDescriptionButton').click(function(){
-                                $('#longDescription').slideToggle( "slow");
-                            });
-                        });
-                    </script>
                 </div>
             </div>
+            <div class="clearfix"></div>
+            <div class="col-md-12">
+                <div class="well well-lg long-description" id="longDescription"><?= $child->long_description ?></div>
+            </div>
+            <div class="clearfix"></div>
         </div>
     </div>
 <?php endif; ?>
