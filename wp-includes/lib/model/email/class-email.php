239a459349aeb3d7d186c5b32455e50dc4db1119
RattleInGlasses <S44k4r@gmail.com>
2015-12-15 00:04:11 +0300

    Изменение email адреса для формы отправки писем

--- wp-includes/lib/model/email/class-email.php
+++ wp-includes/lib/model/email/class-email.php
@@ -2,7 +2,7 @@
 
     class Email
     {
-        const RECEIVER_ADDRESS   = 'S44k4r@gmail.com';
+        const RECEIVER_ADDRESS   = 'info@luchiknadezhdy.ru';
         const MESSAGE_SUBJECT    = 'Сообщение от пользователя сайта www.luchik12.ru';
         const ANONYMOUS_USER_TAG = 'Пользователь не указал имя';
     }
